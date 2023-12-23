b19e9a6b0662fd25944b6240c851e40fc2ac4f44
humphreydev5 <humphreyikhalea@gmail.com>
2023-12-23 04:44:48 -0800

    Create a file app

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# creates a file in /tmp
-file{'/tmp/school':
-	ensure => 'file',
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => 'I love Puppet',
+# This manifest creates a file at /tmp
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
