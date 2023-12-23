7d0ff2b2e10504aa800c71747028a8023085201d
humphreydev5 <humphreyikhalea@gmail.com>
2023-12-23 04:38:23 -0800

    Create a file app

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # creates a file in /tmp
 
 file {'/tmp/school':
-	content =>'I love Puppet',
+	ensure =>'file',
 	mode => '0744',
 	owner => 'www-data',
 	group => 'www-data',
+	content =>'I love Puppet',
 }
