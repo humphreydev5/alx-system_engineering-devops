00ef787955ea7c692409979d0451d69cf595cc8f
humphreydev5 <humphreyikhalea^@gmail.com>
2024-05-07 16:03:59 +0100

    correction for 0-subs.py file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,11 +9,9 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/ubdov_)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return result.get("subscribers")
