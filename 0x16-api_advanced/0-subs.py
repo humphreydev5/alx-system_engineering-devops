972e3cc5952b3f1f6064201ca30b351f49e3cdb5
humphreydev5 <humphreyikhalea^@gmail.com>
2024-05-07 16:08:10 +0100

    correction for 0-subs.py file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
+"""Script that queries subscribers on a given Reddit subreddit."""
+
 
 import requests
-
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
